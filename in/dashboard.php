8e8fd14524111d7234f90e6305b88617ac2f16aa
Todd Murphy <tam1998@comcast.net>
2019-11-10 01:56:58 -0500

    added missing database statements

--- in/dashboard.php
+++ in/dashboard.php
@@ -14,6 +14,19 @@
     
 <?php
     session_start();
+    include("account.php");
+    include("functions.php");
+    
+    $db = mysqli_connect($hostname, $username, $password, $project);
+
+    if (mysqli_connect_errno())
+      {	  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+          exit();
+      }
+    print "Successfully connected to MySQL.<br>";
+    mysqli_select_db($db,$project);
+    
+    
     $email = $_SESSION['email'];
     $firstName = $_SESSION['firstname'];
     $lastName = $_SESSION['lastname'];
