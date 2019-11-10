4f70f71216d023bd96b1703eed8c1cb5f7dc456f
Todd Murphy <tam1998@comcast.net>
2019-11-09 22:08:49 -0500

    fixed id into classes

--- js/dashboardSubMenu.js
+++ js/dashboardSubMenu.js
@@ -3,12 +3,12 @@ var flag = 1;
 $('.showClasses').click(function() {
     console.log(flag);
   if(flag == 1){
-    $('#classMenu').css('display', 'block');
+    $('.classMenu').css('display', 'block');
     flag = 0;
     console.log("display block");
   }
   else{
-    $('#classMenu').css('display', 'none');
+    $('.classMenu').css('display', 'none');
     flag = 1;
   }
 
